da02c08fa35072b6ff4e339c2e9026815a31b307
Artem <artem.makej@bk.ru>
2023-12-06 16:19:03 +0200

    Refactoring func WillConnectTo

--- Netflix App/SceneDelegate.swift
+++ Netflix App/SceneDelegate.swift
@@ -36,48 +36,70 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         
-        let mainAssembly = MainAssembly()
-        let mainViewController = mainAssembly.assemble().wrapInNavigationController()
-        let settingsAssembly = SettingsAssembly()
-        let settingsViewController = settingsAssembly.assemble().wrapInNavigationController()
-        let likesAssembly = LikesAssembly()
-        let likesViewController = likesAssembly.assemble().wrapInNavigationController()
+        let viewControllers = configureViewControllers()
+        let tabBarController = setupTabBarController(with: viewControllers)
         
-        let tabBarController = ImprovedTabBarViewController()
-        tabBarController.themeTintColour = UIColor.tabBarColor
         window = UIWindow(windowScene: windowScene)
         window?.makeKeyAndVisible()
         window?.rootViewController = tabBarController
-        guard let _ = (scene as? UIWindowScene) else { return }
+    }
+    
+    func configureViewControllers() -> [UIViewController] {
+        let mainAssembly = MainAssembly()
+        let settingsAssembly = SettingsAssembly()
+        let likesAssembly = LikesAssembly()
         
+        let mainViewController = mainAssembly.assemble().wrapInNavigationController()
+        let settingsViewController = settingsAssembly.assemble().wrapInNavigationController()
+        let likesViewController = likesAssembly.assemble().wrapInNavigationController()
+        
+        return [mainViewController, likesViewController, settingsViewController]
+    }
+    
+    func setupTabBarController(with viewControllers: [UIViewController]) -> ImprovedTabBarViewController {
+        let tabBarController = ImprovedTabBarViewController()
+        tabBarController.themeTintColour = UIColor.tabBarColor
+        configureTabBarAppearance()
+        
+        setupTabBarItems(for: viewControllers, in: tabBarController)
+        return tabBarController
+    }
+    
+    func setupTabBarItems(for viewControllers: [UIViewController], in tabBarController: UITabBarController) {
+        for (index, viewController) in viewControllers.enumerated() {
+            let title: String
+            let imageName: String
+            switch index {
+            case 0:
+                title = "Главная"
+                imageName = "home"
+            case 1:
+                title = "Любимое"
+                imageName = "likes"
+            case 2:
+                title = "Настройки"
+                imageName = "settings"
+            default:
+                title = ""
+                imageName = ""
+            }
+            
+            let tabBarItem = createTabBarItem(title: title, imageName: imageName, tag: index)
+            viewController.tabBarItem = tabBarItem
+        }
+        
+        tabBarController.viewControllers = viewControllers
+    }
+    
+    func createTabBarItem(title: String, imageName: String, tag: Int) -> UITabBarItem {
+        return UITabBarItem(title: title, image: UIImage(named: imageName), tag: tag)
+    }
+    
+    func configureTabBarAppearance() {
         let appearance = UITabBarItem.appearance()
         let attributes: [NSAttributedString.Key: Any] = [
             .font: UIFont.systemFont(ofSize: 10, weight: .medium)
         ]
         appearance.setTitleTextAttributes(attributes, for: .normal)
-        
-        guard let _ = (scene as? UIWindowScene) else { return }
-        let mainTabBarItem = UITabBarItem(
-            title: "Главная",
-            image: UIImage(named: "home"),
-            tag: 1)
-        
-        mainViewController.tabBarItem = mainTabBarItem
-        let likesTabBarItem = UITabBarItem(
-            title: "Любимое",
-            image: UIImage(named: "likes"),
-            tag: 1)
-        likesViewController.tabBarItem = likesTabBarItem
-        let settingsTabBarItem = UITabBarItem(
-            title: "Настройки",
-            image: UIImage(named: "settings"),
-            tag: 1)
-        settingsViewController.tabBarItem = settingsTabBarItem
-        
-        tabBarController.viewControllers = [
-            mainViewController,
-            likesViewController,
-            settingsViewController
-        ]
     }
 }
